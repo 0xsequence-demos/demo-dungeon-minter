422735c2ddac543b29d6aa8773edb81f0d197c80
moskalyk <morgan.moskalyk@protonmail.ch>
2024-04-11 14:29:57 -0400

    updated variable to check for null

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 
-const PROJECT_ACCESS_KEY = import.meta.env.PROJECT_ACCESS_KEY
+const PROJECT_ACCESS_KEY = import.meta.env.PROJECT_ACCESS_KEY!
 function App() {
   return (
     <>
       <p>demo lootbox</p>
-      <p>{PROJECT_ACCESS_KEY.toString()}</p>
+      <p>{PROJECT_ACCESS_KEY && PROJECT_ACCESS_KEY.toString()}</p>
     </>
   )
 }
