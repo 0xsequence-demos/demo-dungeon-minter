070d1e321355cb8b6eb0bc9415aa4030810378c6
moskalyk <morgan.moskalyk@protonmail.ch>
2024-05-09 07:46:10 -0400

    updated base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/demo-lootbox/",
+  base: "/",
   build: {
     outDir: 'dist',
     rollupOptions: {
-      // additional options can be configured here
     }
   }
 })
