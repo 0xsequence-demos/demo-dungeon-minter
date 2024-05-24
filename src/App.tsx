734f9d630c1abd1e8dc89d4f0298f9f7f300ab29
moskalyk <morgan.moskalyk@protonmail.ch>
2024-05-23 20:44:59 -0400

    fixed opacity of logos

--- src/App.tsx
+++ src/App.tsx
@@ -533,8 +533,8 @@ function App() {
             <div style={{position: 'fixed', bottom: '22px', left: '50%', transform: 'translateX(-50%)', width: '63%'}}>
             {
               !isMobileDevice() ? 
-              <p style={{fontSize: '15px', color: '#555555ba'}} className="content">NETWORK POWERED BY <span style={{color: 'white',opacity: 0.7}}>ARBITRUM NOVA</span> / EMBEDDED WALLET POWERED BY <span style={{color: 'white'}}>SEQUENCE</span> / ARTWORK GENERATION WITH <span style={{color: 'white'}}>SCENARIO.GG</span></p>
-              : <p style={{fontSize: '15px', color: '#555555ba'}} className="content">NETWORK POWERED BY <span style={{color: 'white',opacity: 0.7}}>ARBITRUM NOVA</span> / <br/>EMBEDDED WALLET POWERED BY <span style={{color: 'white'}}>SEQUENCE</span> / <br/> ARTWORK GENERATION WITH <span style={{color: 'white'}}>SCENARIO.GG</span></p>
+              <p style={{fontSize: '15px', color: '#555555ba'}} className="content">NETWORK POWERED BY <span style={{color: 'white',opacity: 0.7}}>ARBITRUM NOVA</span> / EMBEDDED WALLET POWERED BY <span style={{color: 'white',opacity: 0.7}}>SEQUENCE</span> / ARTWORK GENERATION WITH <span style={{color: 'white',opacity: 0.7}}>SCENARIO.GG</span></p>
+              : <p style={{fontSize: '15px', color: '#555555ba'}} className="content">NETWORK POWERED BY <span style={{color: 'white',opacity: 0.7}}>ARBITRUM NOVA</span> / <br/>EMBEDDED WALLET POWERED BY <span style={{color: 'white',opacity: 0.7}}>SEQUENCE</span> / <br/> ARTWORK GENERATION WITH <span style={{color: 'white',opacity: 0.7}}>SCENARIO.GG</span></p>
              }
             </div>
           </>
