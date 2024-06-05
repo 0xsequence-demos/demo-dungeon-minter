767b169e594262246ae86f0de38af8c8dec19862
Taylan Pince <taylanpince@gmail.com>
2024-06-05 12:34:11 +0200

    Update vite config to fix root path for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/demo-dungeon-minter/",
   build: {
     outDir: 'dist',
     rollupOptions: {
